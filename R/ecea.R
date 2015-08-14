e81b3d02856bf118f55c4abd8ad7718d31259ab3
Simon Hickinbotham <sjh518@york.ac.uk>
2015-08-14 15:34:46 +0100

    working version of Ed's version (needs testing)

--- R/ecea.R
+++ R/ecea.R
@@ -39,56 +39,62 @@ ecea.activity <- function(x, scale=FALSE){
 
     # a = population^2 for all species
     pop <- x@tracks
+    
+    #remove seed species (should have 'if(remove_seed = T)' for this
+    for(i in 1:ncol(pop)){if(pop[1,i]>0)pop[,i]<-0}
+
     pop.squared <- pop * pop
 
     # b = pop change for all (score 1 for every diff adjacent pop size)
     pop.shift <-  rBind(rep(0, ncol(pop)), pop[1:(nrow(pop)-1),])
     pdiff <- pop - pop.shift
-    #NB: This fails with sparse matricespdiff.count <- ifelse(pdiff!=0,1,0)
+
     pdiff.count <- ifelse(pdiff!=0,1,0)
     pdiff.count.matrix <- matrix(pdiff.count,nrow = nrow(pdiff),ncol= ncol(pdiff))
 
-    # TODO: this works for column 1 - need to do it over all coulumns....
-    r1 <- cumsum(pdiff.count.matrix[,1])
+    # TODO: this works for column 1 - need to do it over all columns....
+    #r1 <- cumsum(pdiff.count.matrix[,1])
+    pcumsum <- apply(pdiff.count.matrix,2,cumsum)
+    pmax <- apply(pcumsum,2,max)
 
-    #pdsum <- colSums(pdiff.count.matrix)
+    c <- (pop.squared/pcumsum)-1
 
-    # c = (a/b) - 1 for all species
+    # get rid of the nans:
+    c@x[is.nan(c@x)] = 0
 
-    
+    # TODO: sparsify the zeros: 
 
+    #get the max values
+    d = apply(c,2,max)
 
-
-    # d = max(c)
-    # you'd want to plot d - "super sparse" matrix where each column has only one entry. d can be summed to give overall activity. 
-
+    return(d)
 
     ##########################
     # QNN (for reference):
 
-    # Calculate the total population at each time step (each member of x@tracks is the popdy of a single species)
-	population.per.time <- apply(x@tracks, 1, sum)
+#    # Calculate the total population at each time step (each member of x@tracks is the popdy of a single species)
+#	population.per.time <- apply(x@tracks, 1, sum)
 
-    # Calculate the proportion of each species at each time step
-	species.proportions <- sweep(x@tracks, 1, population.per.time, FUN="/")
+#    # Calculate the proportion of each species at each time step
+#	species.proportions <- sweep(x@tracks, 1, population.per.time, FUN="/")
 
-    # Expected proportions are the proportions at the last time step
-	expected.species.proportions.per.time <- rBind(rep(NA, nspecies(x)), species.proportions[1:(nrow(species.proportions) - 1),])
+#    # Expected proportions are the proportions at the last time step
+#	expected.species.proportions.per.time <- rBind(rep(NA, nspecies(x)), species.proportions[1:(nrow(species.proportions) - 1),])
 
-    # Substact expected from observed
-	diff.expected <- (species.proportions - expected.species.proportions.per.time)
-	diff.expected[diff.expected < 0] <- 0
+#    # Substact expected from observed
+#	diff.expected <- (species.proportions - expected.species.proportions.per.time)
+#	diff.expected[diff.expected < 0] <- 0
 
-    # square any positive result
-	diff.expected <- diff.expected ^ 2
-
-    # scale, if requested as an argument
-	if(identical(scale, TRUE)) diff.expected <- diff.expected * population.per.time
-	output <- as(x, "popdat")
-
-    # return the result as another popdy object
-	output@tracks <- as(diff.expected, "dgCMatrix")
-	return(output)
+#    # square any positive result
+#	diff.expected <- diff.expected ^ 2
+#
+#    # scale, if requested via the 2ne argument
+#	if(identical(scale, TRUE)) diff.expected <- diff.expected * population.per.time
+	
+#    # return the result as another popdy object
+#    output <- as(x, "popdat")
+#	output@tracks <- as(diff.expected, "dgCMatrix")
+#	return(output)
 }
 
 
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# qnn
-R package for measuring evolutionary activity
+# QNN
+R package for measuring evolutionary activity, so called becuase it is a "Quantitative, Non-Neutral" measure. This means that the analysis yields a number that can be used to compare activity between samples, and that the measure is based on the idea that any non-neutral evolutionary change will be detected. 
+
+##TODO:
+
+###General tidy up of the package
+
+###Include sample data
+
+###Add the EClark measure
